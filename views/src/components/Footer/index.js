06200917e585af60fe542a91bf361350cac7ea18
Garves <garves44@gmail.com>
2020-08-06 16:47:14 -0600

    updated footer component added FontAwesome

--- views/src/components/Footer/index.js
+++ views/src/components/Footer/index.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+import {
+  StyledNavbar,
+  StyledNavBrand,
+  StyledNavItems,
+  StyledLink,
+} from "../../styled/Navbar";
 
 const Footer = () => {
-    return (
-        <div>
-            This is the Footer changes are made
+  return (
+    <footer className="main-footer" id="main-footer">
+      <StyledNavbar className="footer-nav">
+        <div className="container clearfix">
+          <StyledNavItems className="bottom-nav">
+            <li>
+              <StyledLink to="/">
+                Copyright <FontAwesomeIcon icon={faCopyright} size="2x" /> 2020
+              </StyledLink>
+            </li>
+            <li>
+              <StyledLink to="https://www.linkedin.com/in/jeremy-collins-209545194/">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" /> Jeremy Collins
+              </StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" /> Taylor Remigi
+              </StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" /> Mario Dubon
+              </StyledLink>
+            </li>
+          </StyledNavItems>
         </div>
-    )
-}
+      </StyledNavbar>
+    </footer>
+  );
+};
 
 export default Footer;
--- views/src/pages/Home.js
+++ views/src/pages/Home.js
@@ -2,19 +2,59 @@ import React from "react";
 
 const Home = () => {
   return (
-    <article className="App-article">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </article>
+    <main className="col-12 col-lg-9 d-flex flex-column">
+      <div className="m-5 row justify-content-around">
+        
+        <div className="col-12 col-md-6 col-xl-3 mb-3">
+          <div className="card">
+          <h4 className="card-header bg-dark text-light">Need to Apply</h4>
+          <ul id="list-toDo" className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="badge badge-primary badge-pill"> 05/28/2020</span>
+              <p className="m-1">Sample task to do</p>
+            </li>  
+          </ul>
+        </div>
+        </div>
+        
+        <div className="col-12 col-md-6 col-xl-3 mb-3">
+          <div className="card">
+          <h4 className="card-header bg-dark text-light">Waiting for Response</h4>
+          <ul id="list-inProgress" className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="badge badge-primary badge-pill"> 05/28/2020</span>
+              <p className="m-1">Sample in progress</p>
+            </li>  
+          </ul>
+        </div>
+        </div>
+        
+        <div className="col-12 col-md-6 col-xl-3 mb-3">
+          <div className="card">
+          <h4 className="card-header bg-dark text-light">Interview Scheduled</h4>
+          <ul id="list-inReview" className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="badge badge-primary badge-pill"> 05/28/2020</span>
+              <p className="m-1">Sample task in Review</p>
+            </li>  
+          </ul>
+        </div>
+        </div>
+        
+        <div className="col-12 col-md-6 col-xl-3 mb-3">
+          <div className="card">
+          <h4 className="card-header bg-dark text-light">Need Follow Up</h4>
+          <ul id="list-done" className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="badge badge-primary badge-pill"> 05/28/2020</span>
+              <p className="m-1">Sample task done</p>
+            </li>  
+          </ul>
+        </div>
+        </div>
+        
+      </div>
+    </main>
   );
 };
 
