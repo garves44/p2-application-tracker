0c199d12080a48ac3d70fb9644c03ae36f12c620
Garves <garves44@gmail.com>
2020-08-06 21:13:34 -0600

    added login components working on login page

---
+++ views/src/components/InputField/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputField = () => {
+  return <div className="inputField"></div>;
+};
+
+export default InputField;
---
+++ views/src/components/SubmitButton/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubmitButton = () => {
+  return <div className="submitButton"></div>;
+};
+
+export default SubmitButton;
