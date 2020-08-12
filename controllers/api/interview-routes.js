cc6ffe62c29e4faa81a5e4bb2e44f4dcf14c8407
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-08-11 19:22:35 -0600

    fix typo in interview-routes.js

--- controllers/api/interview-routes.js
+++ controllers/api/interview-routes.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
                 as: 'job',
                 include: [{
                     model: Resume,
-                    attribtues: ['id', 'resume_name', 'resume_link'],
+                    attributes: ['id', 'resume_name', 'resume_link'],
                     as: 'resume'
                 }]
             }
