ff9da59e3bb3702394556c124da076bba1e1639a
mdubonrs@yahoo.com <mdubonrs@yahoo.com>
2020-08-16 14:16:39 -0600

    removed bcrypt from User.js NO LONGER USING

--- models/User.js
+++ models/User.js
@@ -1,13 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-// const bcrypt = require("bcrypt");
-
-// create User model
-// class User extends Model {
-//   checkPassword(loginPw) {
-//     return bcrypt.compareSync(loginPw, this.password);
-//   }
-// }
 
 class User extends Model {}
 
